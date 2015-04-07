08758bc54b2cbca16b308c10223f383fe5554e53
D. Tsogtbayar <dTsogtbayar123@gmail.com>
2015-04-07 15:56:31 +0800

    Changes to SomeExercises
    Some changes

--- 07-04-2015/SomeExercises.java
+++ 07-04-2015/SomeExercises.java
@@ -60,19 +60,27 @@ public class SomeExercises {
         1000 дотор байгаа бүх анхны тоонуудыг хэвлэ
     */
     private void Ex5() {
-        int limit = 1000;
-        Boolean isPrime = false;
-        for (int a=1; a<=limit; a++) {
-            int p = 0;
-            isPrime = true;
-            for (int x =2; x <= a/2; x++) {
-                if (a % x == 0)  {
-                    isPrime = false;
-                }
-            }
-            if (isPrime) {
+//        int i = 1;
+//        Boolean isPrime = false;
+//        do {
+//            i++;
+//            isPrime = true;
+//            for (int x =2; x <= i/2; x++) {
+//                if (i % x == 0)  {
+//                    isPrime = false;
+//                }
+//                if (isPrime) {
+//                    System.out.println(i);
+//                    break;
+//                }
+//            }
+//        } while (i <= 1000);
+        
+        for (int a = 2; a <= 1000; a++) {
+            int t;
+            t = a / 2;
+            if (a % t == 1) {
                 System.out.println(a);
-                break;
             }
         }
     }
@@ -82,6 +90,47 @@ public class SomeExercises {
         (хуваагчдынхаа нийлбэртэй тэнцүү тоог төгс тоо гэнэ. Жш: 6 = 1 + 2 + 3 тул 6 төгс тоо мөн)
     */
     private void Ex6() {
+        
+    }
+    
+    /*
+        Өгсөн тооны 2-тын бичлэгийг хэвлэ
+    */
+    private String getRem(int num) {
+        int div = num / 2;
+        int rem = num % 2;
+        if (rem == 1 || rem == 0) {
+            
+        }
+        return ""+rem;
+    }
+    private void Ex7() {
+        System.out.print("N: ");
+        int n = in.nextInt();
+        getRem(n);
+        System.out.println();
+    }
+    
+    /*
+        2-тын бичлэгээр өгөгдсөн тоог 10-тын
+        тооллын системд шилжүүл
+    */
+    private void Ex8() {
+        
+    }
+    
+    /*
+        Өгөгдсөн тооны бүх цифрүүдийг үсэглэн хэвлэдэг програм бич.
+        Жишээ нь 154 гэж оруулахад: neg tav dorov гэж хэвлэнэ.
+    */
+    private void Ex9() {
+        
+    }
+    
+    /*
+        Хэрэглэгч төрсөн сар, өдрөө өгөнгүүт ямар ордонд төрснийг хэвлэдэг програм бич.
+    */
+    private void Ex10() {
         
     }
     
@@ -95,6 +144,10 @@ public class SomeExercises {
             System.out.println("\t4. Exercise 4");
             System.out.println("\t5. Exercise 5");
             System.out.println("\t6. Exercise 6");
+            System.out.println("\t7. Exercise 7");
+            System.out.println("\t8. Exercise 8");
+            System.out.println("\t9. Exercise 9");
+            System.out.println("\t10. Exercise 10");
             
             System.out.print(": ");
             choose = in.nextInt();
@@ -118,7 +171,22 @@ public class SomeExercises {
                 case 6:
                     Ex6();
                     break;
+                case 7:
+                    Ex7();
+                    break;
+                case 8:
+                    Ex8();
+                    break;
+                case 9:
+                    Ex9();
+                    break;
+                case 10:
+                    Ex10();
+                    break;
             }
         } while (choose != 0);
+    }
+    public static void main(String[] args) {
+        new SomeExercises();
     }
 }
